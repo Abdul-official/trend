2ade578cd97aa2abce1a5acc3a7d9fe7823e65b8
Abdul-official <abru68877@gmail.com>
2025-08-14 16:17:44 +0530

    lightt

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 import express from "express";
 import Stripe from "stripe";
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
 // ✅ Use your Stripe Secret Key (keep it private)
-const stripe = new Stripe(
-  "sk_test_51RvHRKHzVmYq6Pv1ADwBbRHWZDuofLtS8DihiAJV0BPD55R2048TsYLQIsi8pRUUwNN0p57XPbFiUyG03Hqqcz1P00pKP96ZYG"
-);
+// const stripe = new Stripe(
+//   "sk_test_51RvHRKHzVmYq6Pv1ADwBbRHWZDuofLtS8DihiAJV0BPD55R2048TsYLQIsi8pRUUwNN0p57XPbFiUyG03Hqqcz1P00pKP96ZYG"
+// );
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const FRONTEND_ORIGIN = "https://trend-6.onrender.com";
 
@@ -53,8 +57,10 @@ app.post("/create-checkout-session", async (req, res) => {
 });
 
 // Start server
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
   console.log("✅ Server running on http://localhost:5000");
+  console.log("PORT from env:", process.env.PORT);
 });
 
 
